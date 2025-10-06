cd11bd74fe374dabd32875bca35fedd81524599d
Ben Hamel <16502126+ben-hamel@users.noreply.github.com>
2025-10-05 23:44:03 -0230

    backup

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router";
-import Home from "./components/Home";
+import Home from "../components/Home";
+import { AppProvider } from './provider';
 
 function App() {
 	return (
+<AppProvider>
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Home />} />
 			</Routes>
 		</BrowserRouter>
+</AppProvider>
 	);
 }
 
