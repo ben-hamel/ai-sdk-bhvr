d414a7a5df46c26711762fe97d67a2d9f96dbb7a
Ben Hamel <16502126+ben-hamel@users.noreply.github.com>
2025-11-10 21:06:31 -0330

    update frontend routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import chats from "./modules/chats/chats.routes";
 import users from "./modules/users/users.routes";
+import health from "./modules/health/health.routes";
 
 type Bindings = {
   GOOGLE_GENERATIVE_AI_API_KEY: string;
@@ -8,5 +9,6 @@ type Bindings = {
 };
 
 export const v1 = new Hono<{ Bindings: Bindings }>();
+v1.route("/", health);
 v1.route("/chats", chats);
 v1.route("/users", users);
