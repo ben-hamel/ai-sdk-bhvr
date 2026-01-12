682674f6bee133a4bd1a600a79e4b7f52f70cdae
Ben Hamel <16502126+ben-hamel@users.noreply.github.com>
2026-01-11 20:50:54 -0330

    test stuff

--- server/src/lib/better-auth/options.ts
+++ server/src/lib/better-auth/options.ts
@@ -1,4 +1,5 @@
 import type { BetterAuthOptions } from "better-auth";
+import { oAuthProxy } from "better-auth/plugins";
 
 /**
  * Custom options for Better Auth
@@ -7,6 +8,13 @@ import type { BetterAuthOptions } from "better-auth";
  */
 export const betterAuthOptions: BetterAuthOptions = {
   appName: "ai-sdk-bhvr",
+  plugins: [oAuthProxy()],
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
   /**
    * Base path for Better Auth.
    * @default "/api/auth"
