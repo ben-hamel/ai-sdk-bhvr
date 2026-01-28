cb12a9beff9fddf004675d052c092f95d45b978f
Ben Hamel <16502126+ben-hamel@users.noreply.github.com>
2026-01-27 21:10:42 -0330

    update cloudflare bindings to Env

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,10 +2,7 @@ import { Hono } from "hono";
 import chats from "./modules/chats/chats.routes";
 import health from "./modules/health/health.routes";
 
-type Bindings = {
-  GOOGLE_GENERATIVE_AI_API_KEY: string;
-  DATABASE_URL: string;
-};
+type Bindings = Env;
 
 export const v1 = new Hono<{ Bindings: Bindings }>();
 v1.route("/", health);
