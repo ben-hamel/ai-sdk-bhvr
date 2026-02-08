a9e7d39ae0d8e7c58a7d4a99f5188a6364967d0a
Ben Hamel <16502126+ben-hamel@users.noreply.github.com>
2026-02-07 22:37:49 -0330

    update cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,29 +1,43 @@
+import { env } from "cloudflare:workers";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { v1 } from "./routes";
 import { auth } from "./lib/better-auth/index";
+import { v1 } from "./routes";
 
 const app = new Hono<{ Bindings: Env }>();
 
-// app.use(cors());
 app.use(
-  "/api/auth/*",
+  "/*",
   cors({
-    origin: (_origin, c) => {
-      const allowedOrigin = c.env.CORS_ORIGIN || "";
-      return allowedOrigin;
-    },
+    origin: env.CORS_ORIGIN,
+    allowMethods: ["GET", "POST", "OPTIONS"],
     allowHeaders: ["Content-Type", "Authorization"],
-    allowMethods: ["POST", "GET", "OPTIONS"],
-    exposeHeaders: ["Content-Length"],
-    maxAge: 600,
     credentials: true,
   }),
 );
 
-app.on(["GET", "POST"], "/api/auth/*", (c) => {
-  return auth(c.env).handler(c.req.raw);
-});
+app.on(["POST", "GET"], "/api/auth/*", (c) => auth(c.env).handler(c.req.raw));
+
+
+// app.use(cors());
+// app.use(
+//   "/api/auth/*",
+//   cors({
+//     origin: (_origin, c) => {
+//       const allowedOrigin = c.env.CORS_ORIGIN || "";
+//       return allowedOrigin;
+//     },
+//     allowHeaders: ["Content-Type", "Authorization"],
+//     allowMethods: ["POST", "GET", "OPTIONS"],
+//     exposeHeaders: ["Content-Length"],
+//     maxAge: 600,
+//     credentials: true,
+//   }),
+// );
+
+// app.on(["GET", "POST"], "/api/auth/*", (c) => {
+//   return auth(c.env).handler(c.req.raw);
+// });
 
 app.get("/", (c) => c.text("AI SDK BHVR Backend is running"));
 app.route("/api/v1", v1);
